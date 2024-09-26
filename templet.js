f701aea49ef2dfc4432a98ffbfd8875f08325967
b-p-parvez89 <parveztarek4@gmail.com>
2024-09-26 21:47:40 +0600

    understanding sctring

---
+++ templet.js
@@ -0,0 +1,31 @@
+const first="Parvez";
+const last="Tarek";
+const name=first+' '+last;
+
+
+
+const a=23;
+const b=45;
+
+const sum =`The sum of ${a} and ${b} is ${a+b}`;
+console.log(sum);
+
+
+
+const email= `Hello Sir
+ I am Md Parvez Ali and
+ I am from Bangladesh.
+ I am a Flutter developer and I alson know about
+        1. HTML
+        2. CSS
+        3. TAILWIND
+        4. JS
+        5. DOM
+        6. ES6
+        7. DAISY UI
+        8. Flutter
+        9. DART
+        10. GIT AND GITHUB
+
+`;
+console.log(email);
