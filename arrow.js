6f05e3e24c9ac3d7061143750c5cd99e5cc7dc19
b-p-parvez89 <parveztarek4@gmail.com>
2024-09-26 22:33:16 +0600

    fuction_names_and_example

---
+++ arrow.js
@@ -0,0 +1,32 @@
+//fucntion declaration
+
+// function add(a,b){
+//     const result=a+b;
+//     return result;
+
+// }
+
+// console.log(add(12,45,));
+
+function add(a,b){
+  
+    return a+b;
+
+}
+
+console.log(add(12,45,));
+
+
+//fuction expressions
+const sum=function(a,b){
+    return a+b;
+}
+
+console.log(sum(5,58));
+
+
+// arrow fucntion
+
+const add2=(a,b)=>a+b;
+
+console.log(add2(10,20));
